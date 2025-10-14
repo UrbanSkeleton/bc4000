25b1ed60528f0299686f1ae83e78fa3a6511d638
UrbanSkeleton <97252619+UrbanSkeleton@users.noreply.github.com>
2025-10-14 18:03:08 -0400

    Statically links raylib files.

--- build.sh
+++ build.sh
@@ -4,20 +4,22 @@ set -e
 APP_NAME="Battle City 4000"
 BUNDLE="$APP_NAME.app"
 
-clang -Wall main.c -o "$BUNDLE/Contents/MacOS/bc4000" \
+# statically link raylib from /usr/local/include and /usr/local/lib
+clang -Wall main.c -mmacosx-version-min=15.0 \
+  /usr/local/lib/libraylib.a \
   -I/usr/local/include \
-  -L/usr/local/lib \
-  -lraylib \
   -framework CoreFoundation \
   -framework OpenGL \
   -framework Cocoa \
   -framework IOKit \
-  -framework CoreVideo
+  -framework CoreVideo \
+  -o "$BUNDLE/Contents/MacOS/bc4000"
 
 echo "Build complete: $BUNDLE"
 
 chmod +x "$BUNDLE/Contents/MacOS/bc4000" 
 
+# resets highscore when building
 printf '\0\0\0\0\0\0\0\0' > "$BUNDLE/Contents/Resources/hiscore"
 
 ./"$BUNDLE/Contents/MacOS/bc4000"
